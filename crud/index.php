f3c06dc4d9678ead68cd2d33c8476c6960c7d522
Hendrixalb <hendrix.fuentes@hotmail.com>
2017-08-23 16:28:40 -0600

    solucion de botones tabla y export

--- crud/index.php
+++ crud/index.php
@@ -258,25 +258,12 @@
 	<!-- include custom index.js -->
 	<script type="text/javascript" src="custom/js/index.js"></script>
 	
-	
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-		<script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
-		<script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>	
-		<script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
-		<link rel="stylesheet" href="Https://cdn.datatables.net/buttons/1.3.1/js/dataTables.buttons.min.js" />
+<link rel="stylesheet" href="Https://cdn.datatables.net/buttons/1.3.1/js/dataTables.buttons.min.js" />
 		
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/r/dt/jq-2.1.4,jszip-2.5.0,pdfmake-0.1.18,dt-1.10.9,af-2.0.0,b-1.0.3,b-colvis-1.0.3,b-html5-1.0.3,b-print-1.0.3,se-1.0.1/datatables.min.css"/>
  
 <script type="text/javascript" src="https://cdn.datatables.net/r/dt/jq-2.1.4,jszip-2.5.0,pdfmake-0.1.18,dt-1.10.9,af-2.0.0,b-1.0.3,b-colvis-1.0.3,b-html5-1.0.3,b-print-1.0.3,se-1.0.1/datatables.min.js"></script>
 
-
-
-<script src="//cdn.datatables.net/buttons/1.3.1/js/buttons.flash.min.js"></script>	
-
-
-		<link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
 </body>
 </html>
