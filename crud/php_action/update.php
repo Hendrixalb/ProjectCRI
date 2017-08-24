9eef7c303a1ade689a991deebe187d1db639afe0
Ramiro Amaya <ramiro_amaya16@hotmail.com>
2017-08-24 15:16:29 -0600

    Update

--- crud/php_action/update.php
+++ crud/php_action/update.php
@@ -8,15 +8,15 @@ if($_POST) {
 	$validator = array('success' => false, 'messages' => array());
 
 	$id = $_POST['member_id'];
-	$Carnet = $_POST['Carnet'];
-	$Nombres = $_POST['Nombres'];
-	$Apellidos = $_POST['Apellidos'];
-	$Sexo = $_POST['Sexo'];
-	$Cod_ca = $_POST['Cod_ca'];
-	$Trb_gra = $_POST['Trb_gra'];
-	$Fecha_egre = $_POST['Fecha_egre'];
-	$Fecha_grad = $_POST['Fecha_grad'];
-	$active = $_POST['active'];
+	$Carnet = $_POST['EditCarnet'];
+	$Nombres = $_POST['EditNombres'];
+	$Apellidos = $_POST['EditApellidos'];
+	$Sexo = $_POST['EditSexo'];
+	$Cod_ca = $_POST['EditCod_ca'];
+	$Trb_gra = $_POST['EditTrb_gra'];
+	$Fecha_egre = $_POST['EditFecha_egre'];
+	$Fecha_grad = $_POST['EditFecha_grad'];
+	$active = $_POST['Editactive'];
 
 	$sql = "UPDATE estudiantes SET Carnet = '$Carnet', Nombres = '$Nombres', Apellidos = '$Apellidos', Sexo = '$Sexo', Cod_ca = '$Cod_ca', Trb_gra = '$Trb_gra', Fecha_egre = '$Fecha_egre', Fecha_grad = '$Fecha_grad', active = '$active' WHERE id = $id";
 	$query = $connect->query($sql);
