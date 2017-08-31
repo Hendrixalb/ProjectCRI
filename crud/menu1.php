e37a190d84c6074f71dde1bf5ce21e2beb4505a1
Ramiro Amaya <ramiro_amaya16@hotmail.com>
2017-08-31 10:23:22 -0600

    menu

--- crud/menu1.php
+++ crud/menu1.php
@@ -8,7 +8,7 @@
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">Unicaes-CRI</a>
+      <a class="navbar-brand" href="vista.php">Unicaes-CRI</a>
     </div>
     <ul class="nav navbar-nav">
       <li class="active"><a href="#">Inicio</a></li>
@@ -16,7 +16,7 @@
       <li><a href="#"></a></li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
-      <li><a href="">Bienvenido <strong><?php echo $_SESSION['user'];?></strong></a></li>
+      <li><a href="#">Bienvenido <strong><?php echo $_SESSION['user'];?></strong></a></li>
       <li><a href="desconectar.php"><span class="glyphicon glyphicon-log-in"></span> Cerrar Sesion</a></li>
     </ul>
   </div>
